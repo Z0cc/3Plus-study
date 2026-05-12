907668eb1979169454babd89896c5f7acd674b8e
Z0cc <482644910@qq.com>
2026-05-12 15:46:08 +0800

    Embed original english-note grammar

---
+++ docs/.vitepress/theme/components/GrammarOriginal.vue
@@ -0,0 +1,10 @@
+<template>
+  <BackButton />
+  <div class="grammar-original">
+    <iframe
+      title="英语语法"
+      src="https://hzpt-inet-club.github.io/english-note/guide/grammar.html"
+      loading="lazy"
+    />
+  </div>
+</template>
