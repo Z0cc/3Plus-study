bcd2ab7703fad74538e2aaa7de7ec85ba68009fd
Z0cc <482644910@qq.com>
2026-05-12 16:20:51 +0800

    Stop registering removed vocabulary modules

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -4,8 +4,6 @@ import './style.css'
 import HomeLanding from './components/HomeLanding.vue'
 import SubjectSelect from './components/SubjectSelect.vue'
 import EnglishHome from './components/EnglishHome.vue'
-import WordList from './components/WordList.vue'
-import PhraseList from './components/PhraseList.vue'
 import IrregularVerbTable from './components/IrregularVerbTable.vue'
 import GrammarLibrary from './components/GrammarLibrary.vue'
 import GrammarOriginal from './components/GrammarOriginal.vue'
@@ -24,8 +22,6 @@ export default {
     app.component('HomeLanding', HomeLanding)
     app.component('SubjectSelect', SubjectSelect)
     app.component('EnglishHome', EnglishHome)
-    app.component('WordList', WordList)
-    app.component('PhraseList', PhraseList)
     app.component('IrregularVerbTable', IrregularVerbTable)
     app.component('GrammarLibrary', GrammarLibrary)
     app.component('GrammarOriginal', GrammarOriginal)
