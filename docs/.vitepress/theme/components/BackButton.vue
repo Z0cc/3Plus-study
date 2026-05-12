9c1f4340d53ca68530cdcb0bad4d5f3fdd2f77d2
Z0cc <482644910@qq.com>
2026-05-12 15:46:06 +0800

    Add reusable back button

---
+++ docs/.vitepress/theme/components/BackButton.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { withBase } from 'vitepress'
+
+function goBack() {
+  if (typeof window === 'undefined') return
+  if (window.history.length > 1) {
+    window.history.back()
+    return
+  }
+  window.location.href = withBase('/subjects')
+}
+</script>
+
+<template>
+  <button type="button" class="back-button" @click="goBack">返回上一页</button>
+</template>
